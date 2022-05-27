369a418536a84b94b4c10ec488807c0f1f6df392
Scott Lin <scott.lin@tw.synaptics.com>
2022-05-27 23:27:46 +0800

    Update alert message formart

--- src/widget_landing.tsx
+++ src/widget_landing.tsx
@@ -171,7 +171,7 @@ export const Landing = (props: any): JSX.Element => {
         <Alert
           severity="error"
           onClose={() => setAlert(false)}
-          sx={{ marginBottom: "16px" }}
+          sx={{ marginBottom: "16px", whiteSpace: "pre-wrap" }}
         >
           {alertMessage}
         </Alert>
