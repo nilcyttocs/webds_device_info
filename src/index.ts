ee9ba056b066b52355ed3f620fc689f25969906e
Scott Lin <scott.lin@tw.synaptics.com>
2022-07-22 00:08:03 -0700

    Set shadows in widget to indicate scrollable content

--- src/index.ts
+++ src/index.ts
@@ -49,7 +49,7 @@ const plugin: JupyterFrontEndPlugin<void> = {
       },
       execute: () => {
         if (!widget || widget.isDisposed) {
-          const content = new DeviceInfoWidget(service);
+          const content = new DeviceInfoWidget(Attributes.id, service);
           widget = new WebDSWidget<DeviceInfoWidget>({ content });
           widget.id = Attributes.id;
           widget.title.label = Attributes.label;
@@ -62,6 +62,8 @@ const plugin: JupyterFrontEndPlugin<void> = {
         if (!widget.isAttached) shell.add(widget, "main");
 
         shell.activateById(widget.id);
+
+        widget.setShadows();
       }
     });
 
