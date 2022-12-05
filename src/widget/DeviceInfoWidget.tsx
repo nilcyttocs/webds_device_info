f4bb371c202f4c757a87cbe2d46189316634fdfe
Scott Lin <scott.lin@tw.synaptics.com>
2022-12-05 12:48:56 +0800

    Consolidate div elements for widget styling

--- src/widget/DeviceInfoWidget.tsx
+++ src/widget/DeviceInfoWidget.tsx
@@ -18,12 +18,8 @@ export class DeviceInfoWidget extends ReactWidget {
 
   render(): JSX.Element {
     return (
-      <div id={this.id + "_container"} className="jp-webds-widget-container">
-        <div id={this.id + "_content"} className="jp-webds-widget">
-          <DeviceInfoComponent service={this.service} />
-        </div>
-        <div className="jp-webds-widget-shadow jp-webds-widget-shadow-top"></div>
-        <div className="jp-webds-widget-shadow jp-webds-widget-shadow-bottom"></div>
+      <div id={this.id + "_component"}>
+        <DeviceInfoComponent service={this.service} />
       </div>
     );
   }
