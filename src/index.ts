2d4621bb4c043e39eb24eb9989c73c67a51a8bd8
Scott Lin <scott.lin@tw.synaptics.com>
2022-12-05 11:16:07 +0800

    Change rank to 60

--- src/index.ts
+++ src/index.ts
@@ -20,7 +20,7 @@ namespace Attributes {
   export const label = "Device Info";
   export const caption = "Device Info";
   export const category = "Touch - Assessment";
-  export const rank = 50;
+  export const rank = 60;
 }
 
 /**
